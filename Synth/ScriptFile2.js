54554269daea1444470a28498aef39db89d54d03
Antonino Natoli <38180370+Antonino96@users.noreply.github.com>
2018-12-06 12:53:17 +0100

    added a not working cross modulation

--- Synth/ScriptFile2.js
+++ Synth/ScriptFile2.js
@@ -1,7 +1,28 @@
 var context = new AudioContext()
 context.destination.maxChannelCount=2;
-var synth1 = new Synth(context, 5, 3);
 button = document.querySelector('#play');
+var canvas = document.querySelector("#canvas");
+var ctx = canvas.getContext("2d");
+var analyser = context.createAnalyser();
+analyser.fftSize = 4096*2;
+var bufferLength = analyser.frequencyBinCount;
+var dataArray = new Uint8Array(bufferLength);
+analyser.connect(context.destination);
+var synth1 = new Synth(context, 5, 2);
+
+function drawSamples()
+{
+  analyser.getByteTimeDomainData(dataArray);
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+  ctx.beginPath();
+  for(var i=0; i<canvas.width;i++) {
+    ctx.lineTo(i,dataArray[i]);
+  }
+  ctx.stroke();
+  requestAnimationFrame(drawSamples);
+}
+
+drawSamples();
 
  function Oscillator(type, frequency, destination, audioContext){
 
@@ -56,7 +77,7 @@ button = document.querySelector('#play');
     this.context=AudioContext;
 
     this.amplitudeGain = this.context.createGain();
-    this.amplitudeGain.connect(this.context.destination);
+    this.amplitudeGain.connect(analyser);
     this.amplitudeGain.maxChannelCount = 3;
     now = this.context.currentTime;
     this.amplitudeGain.gain.setValueAtTime(0.000001,now);
@@ -69,14 +90,29 @@ button = document.querySelector('#play');
             this.voicesOscillatorsMatrix[i][k].start();
         }
     }
+    this.setCrossModulation = function(){
+            this.voicesOscillatorsMatrix[0][0].oscillator.disconnect(this.amplitudeGain);
+            this.voicesOscillatorsMatrix[1][0].oscillator.disconnect(this.amplitudeGain);
+            this.voicesOscillatorsMatrix[2][0].oscillator.disconnect(this.amplitudeGain);
+            this.voicesOscillatorsMatrix[3][0].oscillator.disconnect(this.amplitudeGain);
+            this.voicesOscillatorsMatrix[4][0].oscillator.disconnect(this.amplitudeGain);
+            this.voicesOscillatorsMatrix[0][0].oscillator.connect(this.voicesOscillatorsMatrix[0][1].oscillator.frequency);
+            this.voicesOscillatorsMatrix[1][0].oscillator.connect(this.voicesOscillatorsMatrix[1][1].oscillator.frequency);
+            this.voicesOscillatorsMatrix[2][0].oscillator.connect(this.voicesOscillatorsMatrix[2][1].oscillator.frequency);
+            this.voicesOscillatorsMatrix[3][0].oscillator.connect(this.voicesOscillatorsMatrix[3][1].oscillator.frequency);
+            this.voicesOscillatorsMatrix[4][0].oscillator.connect(this.voicesOscillatorsMatrix[4][1].oscillator.frequency);               
+    }
 
     this.playSynthNote = function() {
+        setInterval(function(){
+    console.log(synth1.voicesOscillatorsMatrix[0][1].oscillator.frequency.value)
+},10);
         this.attack = Number(document.querySelector("#attack").value);
         this.decay = Number(document.querySelector("#decay").value);
         now = this.context.currentTime;
         this.amplitudeGain.gain.setValueAtTime(0.0001, now);
         this.amplitudeGain.gain.exponentialRampToValueAtTime(0.5,now + this.attack);
-        this.amplitudeGain.gain.linearRampToValueAtTime(0.0000001,now + this.attack + this.decay);
+        this.amplitudeGain.gain.linearRampToValueAtTime(0.0000001,now + this.attack + 4);
   }
 
   this.changeOscillatorLevel = function(voiceNumber,oscillatorNumber, value){
@@ -90,9 +126,17 @@ button = document.querySelector('#play');
     }
   }
 
+  this.setOscillator1Frequency = function(value) {
+    for ( i = 0; i < 4 ; i++) {
+        this.voicesOscillatorsMatrix[i][0].changeFreq(value);
+    }
+  }
+
 }
 
 button.onclick = playSynthSound;
 function playSynthSound(data){
     synth1.playSynthNote();
 }
+
+
