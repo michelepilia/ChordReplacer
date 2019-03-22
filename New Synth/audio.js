c4bb3017f59fb7ea208d32cb34950a10ba19a3d3
Antonino96 <10546001@polimi.it>
2019-03-22 16:34:16 +0100

    ADSR sembra essere funzionante. Il problema era dovuto non solo alla concorrenza (evento keyUp prima di fase sustain e quindi c'era cancellazione di gates1 e gates2), ma anche alla visibilita' delle variabili osc1 e osc2.Si e' introdotto l'oggetto note, a cui si associano due oscillatori. E' stato introdotto anche l'array playing notes. Va ancora sistemata la cancellazione delle note da playing notes

--- New Synth/audio.js
+++ New Synth/audio.js
@@ -3,33 +3,12 @@ var c = new AudioContext();
 var gates1 = [];
 var gates2 = [];
 var tones = [];
-var now = c.currentTime;
+var now ;
 keys = "awsedftgyhujkolpòà";
 var keys_elem_array = [];
 var master = c.createGain();
-var oscillators = [];
 master.connect(c.destination);
-
-
-var ADSR_values = [0.1/*s*/,0.2 /*s*/,0.5/*gain*/,0.2/*s*/]; 
-
-document.onkeydown = function(e){
-  if(keys.includes(e.key) && !e.repeat){
-    console.log(e.key);
-    attack(tones[keys.indexOf(e.key)]);
-  }
-  else{
-    console.log("a");
-  }
-}
-
-document.onkeyup = function(e){
-  if(keys.includes(e.key)){
-    release(tones[keys.indexOf(e.key)]);
-  }
-}
-
-
+var playingNotes = [];
 
 function keyboardMaker() {
   var f = 440;
@@ -41,66 +20,28 @@ function keyboardMaker() {
   }
   //console.log("Ciao");
 }
-
 keyboardMaker();
 
 
-
-
-function attack(freq){
-    if (typeof gates1[freq] != undefined) {
-      console.log("saluti");
-    }
-    var osc1 = c.createOscillator();
-    var osc2 = c.createOscillator();
-    oscillators.push(osc1);
-    oscillators.push(osc2);
-    osc1.type = "sawtooth";
-    osc2.type = "square";
-    var g1 = c.createGain();
-    var g2 = c.createGain();
-    g1.connect(master);
-    g2.connect(master);
-    osc1.connect(g1);
-    osc2.connect(g2);
-    osc1.frequency.value = freq;
-    osc2.frequency.value = 2*freq;
-	  osc1.start();
-	  osc2.start();
-    console.log(osc1.frequency.value);
-    gates1[freq] = g1;
-    gates2[freq] = g2;
-    now=c.currentTime;
-    g1.gain.setValueAtTime(0, now);
-    g2.gain.setValueAtTime(0, now);
-    console.log(sliderAmounts);
-
-    g1.gain.linearRampToValueAtTime(1, now+sliderAmounts[8]/100);
-    g2.gain.linearRampToValueAtTime(1, now+sliderAmounts[8]/100);
-
+function release(note) {
+  /*Should be called once reached point AAAA, and not before*/
+    console.log("RELEASE STARTED");
     now = c.currentTime;
-    g1.gain.exponentialRampToValueAtTime(0.0000001+sliderAmounts[10]/100, now + sliderAmounts[9]/100);
-    g2.gain.exponentialRampToValueAtTime(0.0000001+sliderAmounts[10]/100, now + sliderAmounts[9]/100);
-
-    /*Now sustain. So waits until a keyUp Event, and then release() is called*/
-}
-
-function release(freq) {
-    //console.log(gates1);
-    now = c.currentTime;
-
-    gates1[freq].gain.linearRampToValueAtTime(0, now+sliderAmounts[11]/100);
-    gates2[freq].gain.linearRampToValueAtTime(0, now+sliderAmounts[11]/100);
-    osc1.stop;
-    osc2.stop;
-    osc1.disconnect
-    osc2.disconnect;
-    delete osc1;
-    delete osc2;
-    gates1[freq].disconnect;
-    gates2[freq].disconnect;
-    delete gates1[freq];
-    delete gates2[freq];
+    console.log("gates is: "+gates1);
+    gates1[note.frequency].gain.linearRampToValueAtTime(0, now+sliderAmounts[11]/100);
+    gates2[note.frequency].gain.linearRampToValueAtTime(0, now+sliderAmounts[11]/100);
+    note.oscillator1.stop();
+    note.oscillator2.stop();
+    note.oscillator1.disconnect();
+    note.oscillator2.disconnect();
+    delete note.oscillator1;
+    delete note.oscillator2;
+    gates1[note.frequency].disconnect();
+    gates2[note.frequency].disconnect();
+    delete gates1[note.frequency];
+    delete gates2[note.frequency];
+    delete note;
+    console.log("ENDED RELEASE");
 }
 
 function createButton(n, freq) {
@@ -110,4 +51,91 @@ function createButton(n, freq) {
 	//b.innerHTML = n.toString();
 	b.innerHTML = keys[n];
   b.onclick = function(){attack(freq); b.onmousedown = function(){release(freq)}}
+}
+
+function Note(frequency){
+  this.frequency = frequency;
+  this.hasReachedSustain = false;
+  this.keyUpOccured = false;
+  this.oscillator1 = c.createOscillator();
+  this.oscillator2 = c.createOscillator();
+}
+
+function playNote(note){
+    console.log("EXECUTING playNote: " + note.frequency);
+
+    note.oscillator1.type = "sawtooth";
+    note.oscillator2.type = "sawtooth";
+    var g1 = c.createGain();
+    var g2 = c.createGain();
+    g1.connect(master);
+    g2.connect(master);
+    note.oscillator1.connect(g1);
+    note.oscillator2.connect(g2);
+    note.oscillator1.frequency.value = note.frequency;
+    note.oscillator2.frequency.value = 2*note.frequency;
+    note.oscillator1.start();
+    note.oscillator2.start();
+    gates1[note.frequency] = g1;
+    gates2[note.frequency] = g2;
+
+    now=c.currentTime;
+    g1.gain.setValueAtTime(0, now);
+    g2.gain.setValueAtTime(0, now);
+
+    g1.gain.linearRampToValueAtTime(1, now+sliderAmounts[8]/100);
+    g2.gain.linearRampToValueAtTime(1, now+sliderAmounts[8]/100);
+
+    now = c.currentTime;
+    g1.gain.linearRampToValueAtTime(sliderAmounts[10]/100, now + sliderAmounts[9]/100);
+    g2.gain.linearRampToValueAtTime(sliderAmounts[10]/100, now + sliderAmounts[9]/100);
+    note.hasReachedSustain = true;
+    console.log("SUSTAIN REACHED FOR NOTE: "+note.frequency); 
+    if (note.keyUpOccured) {
+      console.log("Already keyUp Event, occured during attack decay phase: " + note.frequency);
+      release(note);
+    }
+    updatePlayingNotes(note);
+    console.log("NOT EVEN KEYUP EVENT FOR NOTE: "+note.frequency);
+}
+
+document.onkeydown = function(e){
+  if(keys.includes(e.key) && !e.repeat){
+    note = new Note(tones[keys.indexOf(e.key)])
+    playingNotes.push(note);
+    playNote(note);
+  }
+  else{
+    console.log("a");
+  }
+}
+
+document.onkeyup = function(e){
+  console.log("KEY UP EVENT OCCURED...")
+  if(keys.includes(e.key)){
+    console.log("It's a VALID KEY");
+    var a = noteIsPlaying(tones[keys.indexOf(e.key)]);
+    console.log("Element position in playingNotes is: "+a);
+    if (a!=-1) {
+        console.log(playingNotes[a]);
+        playingNotes[a].keyUpOccured=true;
+        release(playingNotes[a]);
+    }
+  }
+}
+
+function noteIsPlaying(frequency){
+  for (var i = 0; i < playingNotes.length; i++) {
+    if (playingNotes[i].frequency==frequency) {
+      return i;
+    }
+  }
+  return -1;
+
+}
+
+function updatePlayingNotes(note){
+  console.log("UPDATE CALLED");
+  playingNotes[noteIsPlaying(note.frequency)]=note;
+  console.log(playingNotes);
 }
