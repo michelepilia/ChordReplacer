cce1a522bfe5ff3b9d570c0a2a74b732fe7390b6
michelepilia <michele.pilia@outlook.it>
2019-05-01 23:51:23 +0200

    Uniti html e configurato firebase

---
+++ db/db.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyD6jsRjiBmcggDBNpYpfsB3ifvlzxwBELs",
+  authDomain: "chord-replacer.firebaseapp.com",
+  databaseURL: "https://chord-replacer.firebaseio.com",
+  projectId: "chord-replacer",
+  storageBucket: "chord-replacer.appspot.com",
+  messagingSenderId: "5808271153"
+};
+firebase.initializeApp(firebaseConfig);
