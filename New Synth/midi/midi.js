5535c53923c55735aa10d25b0995ab49708ec55b
Antonino96 <10546001@polimi.it>
2019-05-18 15:54:24 +0200

    .

--- New Synth/midi/midi.js
+++ New Synth/midi/midi.js
@@ -12,26 +12,11 @@ if (navigator.requestMIDIAccess) {
 // Function executed on successful connection
 function onSuccess(interface) {
  
-
- 
-  // Grab an array of all available devices
+   // Grab an array of all available devices
   var iter = interface.outputs.values();
   for (var i = iter.next(); i && !i.done; i = iter.next()) {
     outputs.push(i.value);
   }
- 
-  // Craft 'note on' and 'note off' messages (channel 3, note number 60 [C3], max velocity)
-  noteon = [0x92, 60, 127];
-  noteoff = [0x82, 60, 127];
- 
-  // Send the 'note on' and schedule the 'note off' for 1 second later
-  outputs[0].send(noteon);
-  setTimeout(
-    function() {
-      outputs[0].send(noteoff);
-    },
-    1000
-  );
   updateOutputsView(outputs);
 }
  
